ac34bfec3c38b4f5a4b7a9f67615040bd6add81b
pjanov <pjanov84@mail.ru>
2023-10-08 20:23:52 +0300

    test app

--- src/main/java/pjanov/homework/hw2/Main.java
+++ src/main/java/pjanov/homework/hw2/Main.java
@@ -1,11 +1,12 @@
 package pjanov.homework.hw2;
 
+import pjanov.homework.hw2.client.ClientGUI;
 import pjanov.homework.hw2.server.ServerGUI;
 
 public class Main {
     public static void main(String[] args) {
         ServerGUI serverGUI = new ServerGUI("Server");
-//        new ClientGUI(serverGUI, "Client");
-//        new ClientGUI(serverGUI, "Client").setLocation(883, 100);
+        new ClientGUI(serverGUI, "Client");
+        new ClientGUI(serverGUI, "Client").setLocation(883, 100);
     }
 }
--- src/main/java/pjanov/homework/hw2/interfaceForm/InterfaceForForm.java
+++ src/main/java/pjanov/homework/hw2/interfaceForm/InterfaceForForm.java
@@ -2,7 +2,10 @@ package pjanov.homework.hw2.interfaceForm;
 
 public interface InterfaceForForm {
     int WIDTH = 400, HEIGHT = 400;
+
     void windowData();
+
     void positionButtons();
+
     void positionTextField();
 }
